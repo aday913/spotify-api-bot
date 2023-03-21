7ee8a03fd74e7953a3ea0de6bced65c9244da3bd
Alex Day <alexanderday@email.arizona.edu>
2023-03-20 19:18:57 -0700

    Create simple logic flow to find artists

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import json
+import logging
+import yaml
 
 import spotipy
 
@@ -8,9 +10,22 @@ print('Grabbing authentication from spotify...')
 spotify = get_oauth()
 print('Successfully connected to the api!')
 
-print('Getting my saved tracks from the api')
-data = spotify.current_user_saved_tracks()
+artist_list = []
+offset = 0
+while True:
+    data = spotify.playlist_tracks('5WQ3UPX8rxzDaIdC1JjNlg', limit=11, offset=offset, fields='items.track.artists')
+    if data['items'] == []:
+        break
+    for i in data['items']:
+        artist = i['track']['artists'][0]['name']
+        if artist not in artist_list:
+            artist_list.append(artist)
+    offset += 10
 
-with open('sample_data.json', 'w') as outfile:
+print(artist_list)
+print(len(artist_list))
+
+
+with open('/data/sample_data.json', 'w') as outfile:
     print('Writing data to an output json file')
-    json.dump(data, outfile)
+    json.dump(data, outfile, indent=4)
